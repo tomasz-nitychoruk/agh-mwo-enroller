58b94d3f3c5182e6fcc27e07df6fcd45a91ff466
tomasz-nitychoruk <tafnitychoruk@gmail.com>
2022-04-03 11:56:52 +0200

    additions

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,8 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -22,4 +24,25 @@ public class ParticipantService {
 		return query.list();
 	}
 
+	public Participant findByLogin(String login){
+		return (Participant) connector.getSession().get(Participant.class, login);
+	}
+
+	public void add(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	}
+
+	public void delete(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+	}
+
+	public void update(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().merge(participant);
+		transaction.commit();
+	}
 }
